e0638e3fa8aa5d73dd87cf35f8c5ad3dbe5d3ab8
Agata Pacholczyk <agataa.pacholczyk@gmail.com>
2020-04-09 19:23:57 +0200

    add active element in script.js

--- js/script.js
+++ js/script.js
@@ -8,31 +8,35 @@
     console.log('Link was clicked!');
     console.log(event);
   
-    /* [DONE] remove class 'active' from all article links  */
+    /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }   
   
-    /* [DONE] add class 'active' to the clicked link */
+    /* add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
   
-    /* [DONE] remove class 'active' from all articles */
+    /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }  
     /* get 'href' attribute from the clicked link */
-    const href = clickedElement.getAttribute('href');
-    console.log('href is', href);
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log('articleSelector:', articleSelector);
   
     /* find the correct article using the selector (value of 'href' attribute) */
-   
+    const targetArticle = document.querySelector(articleSelector);
+    console.log('targetArticle:', targetArticle);
+    
     /* add class 'active' to the correct article */
-}
+    targetArticle.classList.add('active');
+    console.log('targetArticle:', targetArticle);
+  }
   
   const links = document.querySelectorAll('.titles a');
   
